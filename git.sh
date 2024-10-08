f1261c0b582675e4fc7f77636525c36fd2f9af51
Yalamanchili Surya Teja <suryatejaprogramming@gmail.com>
2024-10-08 11:17:39 +0530

    deployment commit

--- git.sh
+++ git.sh
@@ -1,6 +1,3 @@
-git init
 git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/teja1103/biryani-darbaar-client.git
+git commit -m "deployment commit"
 git push -u origin main
