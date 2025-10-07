fd3bdca3aaf254b85397eaa770827f871c05f0c0
Sarthak S Kumar <sarthakskumar7@gmail.com>
2025-10-07 23:45:38 +0530

    refactor(App.css): add App.css,App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./styles/App.css";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./Reusable-components/Navbar";
 import Home from "./Components/Home";
 import Menu from "./Components/Menu";
@@ -16,6 +16,23 @@ import { useState, useEffect } from "react";
 import { auth } from "./lib/firebase";
 import SignInSignUpModal from "./Reusable-components/login";
 import Contact from "./Components/Contact";
+import Checkout from "./Reusable-components/order-component/Checkout";
+import { CartProvider } from "./Components/CartProvider";
+
+// Scroll to top component
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'instant'
+    });
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -32,38 +49,41 @@ function App() {
   return (
     <>
       <Router>
-        <div className="flex flex-col min-h-screen ">
-          <Navbar />
-          <div className="flex-1">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/About" element={<About />} />
-              <Route path="/Menu" element={<Menu />} />
-              <Route path="/SpecialOffer" element={<SpecialOffer />} />
-              <Route path="/Contact" element={<Contact />} />
-              <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-              <Route
-                path="/termsandconditions"
-                element={<TermsAndConditions />}
-              />
-              <Route
-                path="/Order"
-                element={
-                  isAuthenticated ? (
-                    <Order />
-                  ) : (
-                    <SignInSignUpModal
-                      onClose={() => {}}
-                      onSuccess={() => {}}
-                    />
-                  )
-                }
-              />
-              {/* <Route path="/Order" element={<Order />} /> */}
-            </Routes>
+        <ScrollToTop />
+        <CartProvider>
+          <div className="flex flex-col min-h-screen relative">
+            <Navbar />
+            <div className="flex-1">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/About" element={<About />} />
+                <Route path="/Menu" element={<Menu />} />
+                <Route path="/SpecialOffer" element={<SpecialOffer />} />
+                <Route path="/Contact" element={<Contact />} />
+                <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+                <Route
+                  path="/TC"
+                  element={<TermsAndConditions />}
+                />
+                <Route
+                  path="/Order"
+                  element={
+                    isAuthenticated ? (
+                      <Order />
+                    ) : (
+                      <SignInSignUpModal
+                        onClose={() => { }}
+                        onSuccess={() => { }}
+                      />
+                    )
+                  }
+                />
+                <Route path="/checkout" element={<Checkout />} />
+              </Routes>
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
+        </CartProvider>
       </Router>
     </>
   );
